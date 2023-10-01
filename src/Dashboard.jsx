f413014363f6c3a8fa8519edf590a3b494774293
theo <lesgooo1993@proton.me>
2023-10-01 16:03:02 +0530

    class cleanup

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -93,14 +93,14 @@ export default ({ffmpeg}) =>{
     }
 
     return(
-        <div className="parent xl:border-gray-400 xl:shadow-lg xl:p-4 xl:rounded-lg flex flex-col gap-2">
-            <div className="top flex flex-col gap-2 xl:gap-0 xl:flex-row xl:justify-between">
+        <div className="parent p-4 gap-2 flex flex-col xl:border-gray-400 xl:shadow-lg xl:rounded-lg">
+            <div className="top flex flex-col gap-2 xl:gap-0 xl:flex-row">
                 <h2 className="font-bold text-2xl">Dashboard</h2>
-                <input className="xl:text-right file:rounded file:border-0 xl:file:mr-2 file:bg-blue-500 file:text-white file:py-1 file:px-2"
+                <input className="file:rounded file:border-0 xl:file:ml-2 file:bg-blue-500 file:text-white file:py-1 file:px-2"
                     ref={fileRef} type="file" accept='video/*' />
             </div>
 
-            <div className='flex flex-col xl:flex-row'>
+            <div className='flex flex-col xl:flex-row xl:gap-2'>
                 <div className="p-2 flex flex-col justify-between">
                     <div className="top">
                         <h3 className="text-lg font-bold"> Input Video stream</h3>
@@ -111,40 +111,40 @@ export default ({ffmpeg}) =>{
                     <div>
                         <label>Start Position</label>
                         <div className="flex gap-1">
-                            <input className="border rounded text-center [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                            <input className="grow border rounded text-center [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
                             type="number" value={startTime} onChange={e=>setStartTime(parseInt(e.target.value))}/>
                             <button className="text-sm px-2 py-1 rounded bg-blue-500 text-white" onClick={()=>setTimes(0)}>Current Position</button>
                         </div>
                     </div>
 
                     <div>
-                        <label>End Position</label> <br/>
-                        <div className='flex gap-2'>
-                            <input className='border rounded text-center [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none'
+                        <label>End Position</label>
+                        <div className='flex gap-1'>
+                            <input className='grow border rounded text-center [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none'
                             type="number" value={endTime} onChange={e=>setEndTime(parseInt(e.target.value))} />
                             <button className="text-sm px-2 py-1 rounded bg-blue-500 text-white" onClick={()=>setTimes(1)} >Current Position</button>
                         </div>
                     </div>
                     
-                    <div className='flex gap-2'>
+                    <div className='flex gap-1 mt-1'>
                         <button className="grow rounded px-2 py-1 bg-blue-500 text-white" onClick={()=>extractVideo("time")}>Extract</button>
                         <button className="grow rounded px-2 py-1 bg-blue-500 text-white" onClick={()=>extractVideo("full")}>Extract Full Video</button>
                     </div>
                 </div>
                 </div>
 
-                <div className='p-2 flex flex-col justify-between gap-2'>
+                <div className='p-2 flex flex-col justify-between'>
                     <h3 className='text-lg font-bold'>Result</h3>
 
-                    <div className='flex flex-col gap-1'>
-                        <p>Audio</p>
-                        <audio className="rounded-md" ref={outaudioRef} controls></audio><br/>
+                    <div>
+                        <p className='font-bold'>Audio</p>
+                        <audio className="mb-2 rounded-md" ref={outaudioRef} controls></audio>
                         {audiodownload && <a className='text-center rounded px-2 py-1 bg-blue-500 text-white' href={audiourl} download={"audio_output.mp3"}>Download Only Audio</a>}
                     </div>
 
-                    <div className="flex flex-col gap-1">
-                        <p>Video</p>
-                        <video ref={outputVidRef} className="rounded-md" controls width="300" height="300" ></video><br/>
+                    <div>
+                        <p className='font-bold'>Video</p>
+                        <video ref={outputVidRef} className="mb-2 rounded-md" controls width="300" height="300" ></video>
                         {videodownload && <a className='text-center rounded px-2 py-1 bg-blue-500 text-white' href={videourl} download={"Video_output.mp4"}>Download Only Video</a>}
                     </div>
                 </div>
